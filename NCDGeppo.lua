77d709c3a8856b8d815ebb15be4541914874093a
HecatiA <88549714+LiterallyATrueClown@users.noreply.github.com>
2023-06-30 21:23:58 +0700

    Update NCDGeppo.lua

--- NCDGeppo.lua
+++ NCDGeppo.lua
@@ -1,14 +1,13 @@
-for i, v in pairs(getgc()) do 
-                        if type(v) == "function" and getfenv(v).script == game.Players.LocalPlayer.Character:WaitForChild("Skyjump") then 
-                            for i2,v2 in next, getupvalues(v) do
-                                spawn(function()
-                                    ncdgeppo = game:GetService("RunService").RenderStepped:Connect(function() 
-                                        if tostring(v2) == "0" then
-                                            setupvalue(v,i2,0/0)
-                                        end
-                                    end)
-                                end)
-                                end
-                            end
-                        end
-                    end
+for i, v in pairs(getgc()) do
+  if type(v) == "function" and getfenv(v).script == game.Players.LocalPlayer.Character:WaitForChild("Skyjump") then
+    for i2,v2 in next, getupvalues(v) do
+      spawn(function()
+      game:GetService("RunService").RenderStepped:Connect(function()
+      if tostring(v2) == "0" then
+        setupvalue(v,i2,0/0)
+      end
+      end)
+      end)
+    end
+  end
+end
